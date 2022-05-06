2b79839d56eba6e19cd0b7abc3b9ffcac84f0467
alecksv <aleck.svetlovich@gmail.com>
2022-05-06 14:29:42 +0300

    check view

--- docs/view.js
+++ docs/view.js
@@ -12,7 +12,7 @@ export class HeaderView {
             <a href="#" title="home">
               <object
                 type="image/svg+xml"
-                data="../img/logo hichat_elips.svg"
+                data="../img/logo_hichat_elips.svg"
                 width="79"
                 height="48"
               >
